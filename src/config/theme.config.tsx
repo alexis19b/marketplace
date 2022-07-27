6249f28a252a0073125abe788595f6b8e0323e48
Jesus Alexis <ingjesusalexis@gmail.com>
2022-07-27 07:52:38 -0400

    Customizacion theme

---
+++ src/config/theme.config.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+
+type ThemeProp = {
+  children: JSX.Element
+}
+
+export enum themePalette {
+  BG = "#12181b",
+  LIME = "#C8FA5F",
+  FONT_GLOBAL = "'JetBrains Mono', monospace",
+}
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: themePalette.BG,
+    },
+    primary: {
+      main: themePalette.LIME,
+    }
+  },
+  typography: {
+    fontFamily: themePalette.FONT_GLOBAL
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        style: {
+          textTransform: "none",
+          boxShadow: "none",
+          borderRadius: "0.5em"
+        },
+      }
+    }
+  }
+
+})
+
+export const Themeconfig: React.FC<ThemeProp> = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  )
+}
