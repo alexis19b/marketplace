df199c6446c9015f3dc5826b3e0d22b48f903b45
Jesus Alexis <ingjesusalexis@gmail.com>
2022-07-31 14:42:18 -0400

    context added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,17 @@
 import { Button, Container } from '@mui/material'
 import React from 'react'
+import { useNotification } from '../../context/notification.context'
 
 export const HomePage: React.FC<{}> = () => {
+  const { getError } = useNotification();
+
+  const handleClick = () => {
+    getError("Enviados desde el context")
+  }
+
   return (
     <Container sx={{ mt: 9 }} maxWidth="xl">
-      <Button variant="contained">Home</Button>
+      <Button onClick={handleClick} variant="contained">Home</Button>
     </Container>
   )
 }
